5f219c405d4f78872d68f2915020680531c112bb
Jianqiu Wang <jianqiuwang1@icloud.com>
2022-06-23 22:51:55 -0500

    commit tree e40ef718cd46f93ad59236e8b49664a7950f02de

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+// function findMatching(arrayOfDrivers, target){
+//     return arrayOfDrivers.filter((driver)=>driver.toUpperCase()===target.toUpperCase())
+// }
+
+function fuzzyMatch(collection, result){
+    return collection.filter((name)=>name.substring(0,2) === result.substring(0,2))
+    
+       
+}
+
+function matchName(arrayOfDriversObj, res){
+    return arrayOfDriversObj.filter((element)=>element['name']===res)
+    
+}
+
+function findMatching(arrayOfDrivers, target){
+    return arrayOfDrivers.filter((driver)=>driver.toLowerCase()===(target).toLowerCase())
+}
+
